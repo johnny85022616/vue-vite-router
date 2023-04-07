dd0259189c7a36c2cb02cc0f34394071386315ee
johnny_chang <johnny_chang@friday.tw>
2023-04-07 14:21:21 +0800

    新增刪除天數方法

--- src/utils/tools.js
+++ src/utils/tools.js
@@ -4,6 +4,12 @@ export default{
     const d = new Date(date)
     d.setDate(d.getDate() + days)
     return d 
+  },
+  minusDay(date , days){
+    date.replace(/(\d{4})\/(\d{2})\/(\d{2})\s(\d{2}):(\d{2}):(\d{2})/,'$1-$2-$3T$4:$5:$6')
+    const d = new Date(date)
+    d.setDate(d.getDate() - days)
+    return d 
   }
 }
 
