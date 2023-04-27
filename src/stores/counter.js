c8dc32a1326c76d0e476dac14dc9fc58a2321f46
johnny_chang <johnny_chang@friday.tw>
2023-04-27 16:17:46 +0800

    新增pinia非同步請求及 storeToRefs 狀態解構

--- src/stores/counter.js
+++ src/stores/counter.js
@@ -3,9 +3,21 @@ import { computed, ref } from 'vue';
 
 export const useCounterStore = defineStore('counter',()=>{
   let count = ref(0)
+  const product = ref(null)
 
   function addCount(){
     count.value++
+  }
+
+  async function fetchApiData() {
+      const data = await fetch(
+        "https://m.shopping.friday.tw/mobileapi/api/product/7995418/detail?cid=419485"
+      )
+      .then((res) => res.json())
+      .then((result) => { 
+        return result.data[0]
+      });
+      product.value = data
   }
 
   const doubleCount = computed(()=>{
@@ -14,7 +26,9 @@ export const useCounterStore = defineStore('counter',()=>{
 
   return {
     count ,
+    product,
     addCount,
-    doubleCount
+    doubleCount,
+    fetchApiData
   }
 })
--- src/views/vue3/pinia/index.vue
+++ src/views/vue3/pinia/index.vue
@@ -1,16 +1,21 @@
 <template>
   <div class="pinia">
-    <h2>count: {{store.count}}</h2>
-    <button @click="handleClick">plus</button>
+    <h2>count: {{count}}</h2>
+    <button @click="handlePlus">plus</button>
+    <h2>Product</h2>
+    <button @click="store.fetchApiData">product Fetch</button>
+    {{product}}
   </div>
 </template>
 
 <script setup>
+import { storeToRefs } from 'pinia';
 import {useCounterStore} from '../../../stores/counter';
 
 const store = useCounterStore()
+const {product , count } = storeToRefs(store)  //將product & count 從store解構出來 
 
-function handleClick(){
+function handlePlus(){
   store.addCount();
 }
 
