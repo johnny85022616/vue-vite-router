84416c68e416032c7b45fe4a14ced3eb744c0d9f
johnny_chang <johnny_chang@friday.tw>
2024-02-29 18:16:56 +0800

    新增v-model測試

---
+++ src/views/vue3/componentVmodel/customerDom.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="customInput">
+    <p>分數{{point}}</p>
+    <button>+1000</button>
+  </div>
+</template>
+
+<script setup>
+import { computed } from "vue"
+
+  const p = defineProps(['point'])
+  const up = defineEmits(['update:modelPoint'])
+
+
+</script>
+
+<style scoped>
+
+</style>
--- src/views/vue3/componentVmodel/index.vue
+++ src/views/vue3/componentVmodel/index.vue
@@ -3,14 +3,21 @@
     <h1>component v-model</h1>
     <customInput v-model="inputVal"/>
     <p>input值: {{inputVal}}</p>
+    <customerDom v-model="point"/>
   </div>
 </template>
 
 <script setup>
   import { ref } from 'vue';
 import customInput from './customInput.vue';
+import customerDom from './customerDom.vue';
 
   const inputVal = ref('')
+  const point = ref(0)
+
+  function updatePoint(plusPoint){
+    point.value += plusPoint
+  }
 </script>
 
 <style scoped>
