9470eddf5c0067b09257c53070d0e91112552f3f
johnny_chang <johnny_chang@friday.tw>
2024-03-21 11:32:59 +0800

    ref 及 defineExpose運用

---
+++ src/views/vue3/defineExpose/childRef.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="child">
+    <h2>子組件Ａ</h2>
+    <p>count:{{money}}</p>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+  const money = ref(10000)
+
+  function minusTenDollars(){
+    money.value -= 10
+  }
+
+  //expose 給父類別使用
+  defineExpose({
+    money,
+    minusTenDollars
+  });
+
+</script>
+
+<style scoped>
+
+</style>
--- src/views/vue3/defineExpose/index.vue
+++ src/views/vue3/defineExpose/index.vue
@@ -1,22 +1,28 @@
 <template>
   <div class="defineExpose">
     <h1>DefineExpose</h1>
-    <p>Title: {{title}}</p>
-    <button @click="handleClick">use child</button>
-    <child ref="childComponentRef"></child>
+    <h1>父組件</h1>
+    <p>money:{{money}}</p>
+    <button @click="handleClick">和子組件借10塊</button>
+    <child ref="sonA"></child>
   </div>
 </template>
 
 <script setup>
+/* 功能
+父組件取得子組件參數及方法
+---> 父組件使用ref 子組件使用defineExpose
+*/
 import { ref } from 'vue';
-import child from './child.vue';
+import child from './childRef.vue';
 
 const title = ref('')
-const childComponentRef = ref(null)
+const sonA = ref()
+const money = ref(1000)
 
 function handleClick(){
-  title.value = childComponentRef.value.title  //不需要使用.value
-  childComponentRef.value.increment()
+  money.value+=10
+  sonA.value.minusTenDollars()
 }
 </script>
 
