e11d7e79792d007a667c73a978fefdce26b72266
johnny_chang <johnny_chang@friday.tw>
2024-05-15 15:29:56 +0800

    watch(一般型別)

--- src/views/vue3/watch/index.vue
+++ src/views/vue3/watch/index.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="watch">
     <h1>Watch</h1>
+    <primitiveData></primitiveData>
   </div>
 </template>
 
 <script setup>
-
+import primitiveData from './primitiveData.vue';
 </script>
 
 <style lang="scss" scoped>
---
+++ src/views/vue3/watch/primitiveData.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="primitiveData">
+    <h2>一般型別</h2>
+    <h3>{{count}}</h3>
+    <button @click="plus">+1</button>
+  </div>
+</template>
+
+<script setup>
+import { ref, watch } from "vue";
+
+  const count = ref(0)
+  
+  function plus(){
+    count.value +=1
+  }
+  //一般使用
+  // watch(count ,(newVal, oldVal)=>{
+  //   console.log(newVal , oldVal);
+  // })
+  //條件使用
+  const watchFn = watch(count ,(newVal, oldVal)=>{
+    console.log(newVal , oldVal);
+    if(newVal >= 10 ){
+      watchFn() //重新呼叫會停止監視
+    }
+  })
+</script>
+
+<style lang="scss" scoped>
+
+</style>
