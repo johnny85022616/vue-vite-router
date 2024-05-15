6dbe811a98993e7eabeeb38d66e6110c89ceac55
johnny_chang <johnny_chang@friday.tw>
2024-05-15 11:13:38 +0800

    conputed 新增get&set方法

--- src/views/vue3/computed/index.vue
+++ src/views/vue3/computed/index.vue
@@ -9,7 +9,10 @@
       <label for="">名</label>
       <input type="text" v-model="lastName">
     </div>
-    <h3>全名： {{ fullName }}</h3>
+    <div>
+      <label for="">全名</label>
+      <input type="text" v-model="fullName">
+    </div>
   </div>
 </template>
 
@@ -19,8 +22,23 @@ import { computed, ref } from 'vue';
 const firstName = ref('')
 const lastName = ref('')
 
-const fullName = computed(()=>{
-  return `${firstName.value.toUpperCase()}-${lastName.value.toUpperCase()}`
+//只有使用computed中的get方法
+// const fullName = computed(()=>{
+//   return `${firstName.value.toUpperCase()}-${lastName.value.toUpperCase()}`
+// })
+
+//使用conputed中的set和get方法
+const fullName = computed({  
+  set(val){  //改變fullName時參數會接收到fullname的值
+    const arr = val.split('-')
+    if(arr && arr.length>0){   //fullName轉換為fistName及lastName
+      firstName.value = arr[0].toLowerCase()
+      lastName.value = arr[1].toLowerCase()
+    }
+  },
+  get(){
+    return `${firstName.value.toUpperCase()}-${lastName.value.toUpperCase()}`
+  }
 })
 </script>
 
