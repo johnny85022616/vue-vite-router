24c2ac1143906d2bfc3d5ebc5b97456d78c96ef0
johnny_chang <johnny_chang@friday.tw>
2024-05-16 16:10:23 +0800

    watch 使用ref監視referenceData

--- src/views/vue3/watch/index.vue
+++ src/views/vue3/watch/index.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="watch">
     <h1>Watch</h1>
-    <primitiveData></primitiveData>
+    <primitiveRefData></primitiveRefData>
+    <referenceRefData></referenceRefData>
   </div>
 </template>
 
 <script setup>
-import primitiveData from './primitiveData.vue';
+import primitiveRefData from './primitiveRefData.vue'
+import referenceRefData from './referenceRefData.vue';
 </script>
 
 <style lang="scss" scoped>
---
+++ src/views/vue3/watch/referenceRefData.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="primitiveData">
+    <h2>對象型別</h2>
+    <h3>姓名：{{person.name}}</h3>
+    <h3>年齡：{{person.age}}</h3>
+    <button @click="changeNmae">變更姓名</button>
+    <button @click="changeAge">變更年齡</button>
+    <button @click="changePerson">變更年齡</button>
+  </div>
+</template>
+
+<script setup name="referenceData">
+import { ref, watch } from "vue";
+const person = ref({
+  name: '張三',
+  age: 18 ,
+})
+
+function changeNmae (){
+  person.value.name += '~'
+}  
+function changeAge (){
+  person.value.age += 1
+}
+function changePerson(){
+  person.value = {
+    name: '李四',
+    age: 50,
+  }
+}
+//預設不開啟深度監視，若要深度監視則需要手動開啟
+watch(person, (newVal , oldVal)=>{
+  console.log(newVal, oldVal);
+}, {deep: true})
+
+</script>
+
+
+
+<style lang="scss" scoped>
+
+
+</style>
